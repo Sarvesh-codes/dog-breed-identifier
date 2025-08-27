7103cf0e2898391a18f8500534f4f67232ecc92c
Sarvesh Dhandapani <d.sarvesh2004@gmail.com>
2025-08-27 11:14:23 +0530

    updated port

--- dog_backend/app.py
+++ dog_backend/app.py
@@ -278,5 +278,5 @@ def serve_index():
     return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     serve(app, host="0.0.0.0", port=port)
